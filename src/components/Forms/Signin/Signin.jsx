be5d5ef26ab7f2a64c8dcc660e644ec47e4343af
turboBarbitura <turbobarbitura@mail.ru>
2021-11-27 14:27:58 +0700

    start material UI integration

--- src/components/Forms/Signin/Signin.jsx
+++ src/components/Forms/Signin/Signin.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import s from './signin.module.css'
+import {TextField} from "@mui/material";
+
+
 
 const Signin = () => {
     return (
         <div className={s.signinWrapper}>
             <form className={s.signinForm} action="#">
                 <div className={s.userLogin}>
-                    <input type="text" placeholder='Логин / E-mail'/>
+                    {/*<input type="text" placeholder='Логин / E-mail'/>*/}
+                    <TextField className={s.userLoginI} id="standard-basic-l" label="Логин / E-mail" variant="standard"
+                               InputLabelProps={{className: s.textFieldLogin}}
+                    />
+
 
                 </div>
                 <div className={s.userPass}>
-                    <input type="password" placeholder='Пароль'/>
+                    {/*<input type="password" placeholder='Пароль'/>*/}
+                    <TextField className={s.userPassI} id="standard-basic-p" label="Пароль" variant="standard"
+                        InputLabelProps={{className: s.textFieldPass}}
+                    />
 
                 </div>
                 <p className={s.forgotPass}>Забыли пароль?</p>
